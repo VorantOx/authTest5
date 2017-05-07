50372e16208be4aea9fe885f76731cbb8d281973
Антон Воронов <vorant@gmail.com>
2017-05-07 23:33:01 +0300

    Add project files.

---
+++ authTest5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(authTest5.Startup))]
+namespace authTest5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
